3f4fe3d11eed436eebf870578f1acfffa125ff8c
YaroslavKryuchkov <yaroslav.kryuchkov.vladimirovich@gmail.com>
2022-08-28 19:47:05 +0300

    Добавил свет в первый уровень. Скрыл белые стены в первом уровне. Добавил проточную воду в первый уровень. Добавил морской уровень и море.

---
+++ Assets/Scripts/Sea.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sea : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] int height;
+    [SerializeField] float squareSize;
+
+    void Start()
+    {
+        MakeMesh();
+    }
+
+    void MakeMesh()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        int[] triangles = new int[width * height * 6];
+        Vector3[] vertices = new Vector3[width * height * 4];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                vertices[(x + y * width) * 4]     = new Vector3((-(width / 2) + x    ) * squareSize, 0, (-(height / 2) + y    ) * squareSize);
+                vertices[(x + y * width) * 4 + 1] = new Vector3((-(width / 2) + x    ) * squareSize, 0, (-(height / 2) + y + 1) * squareSize);
+                vertices[(x + y * width) * 4 + 2] = new Vector3((-(width / 2) + x + 1) * squareSize, 0, (-(height / 2) + y + 1) * squareSize);
+                vertices[(x + y * width) * 4 + 3] = new Vector3((-(width / 2) + x + 1) * squareSize, 0, (-(height / 2) + y    ) * squareSize);
+
+                triangles[(x + y * width) * 6]     = (x + y * width) * 4;
+                triangles[(x + y * width) * 6 + 1] = (x + y * width) * 4 + 1;
+                triangles[(x + y * width) * 6 + 2] = (x + y * width) * 4 + 2;
+
+                triangles[(x + y * width) * 6 + 3] = (x + y * width) * 4 + 2;
+                triangles[(x + y * width) * 6 + 4] = (x + y * width) * 4 + 3;
+                triangles[(x + y * width) * 6 + 5] = (x + y * width) * 4;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ Assets/Shaders/CanalizationWater.shader
@@ -0,0 +1,57 @@
+Shader "Unlit/CanalizationWater"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Scale ("Scale", Float) = 5
+		_Speed ("Speed", Float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			float _Scale;
+			float _Speed;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.y = o.uv.y * _Scale - _Time.w * _Speed;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
